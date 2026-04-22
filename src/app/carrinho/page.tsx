c17934d912d17a715f970717558fd40b1adba718
vitor_hugo <vitorhuabmo@gmail.com>
2026-04-22 12:15:17 -0300

    commit

---
+++ src/app/carrinho/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import EnviarPedido from "@/components/EnviarPedido";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [carrinho, setCarrinho] = useState<any>(null);
+
+  useEffect(() => {
+    const storage = localStorage.getItem("carrinho");
+
+    if (storage) {
+      setCarrinho(JSON.parse(storage));
+    } else {
+      setCarrinho({});
+    }
+  }, []);
+
+  if (!carrinho) return <p>Carregando...</p>;
+
+  return (
+  <div className="flex flex-col min-h-screen bg-amber-100">
+
+    <header className="flex justify-between items-center bg-amber-900 text-white px-8 py-6">
+      <h1 className="font-black text-3xl md:text-4xl text-center w-full">
+        Pedidos
+      </h1>
+
+      <Link
+        href="/"
+        className="absolute right-6 top-6 text-sm hover:underline"
+      >
+        Voltar
+      </Link>
+    </header>
+
+    <main className="flex flex-1 items-start justify-center p-6">
+
+      <div className="w-full max-w-2xl bg-white rounded-xl shadow-lg p-6">
+
+        <div className="flex flex-col gap-4">
+          <EnviarPedido />
+        </div>
+
+      </div>
+
+    </main>
+
+    <footer className="bg-amber-900 text-white text-center p-3">
+      © 2025-{new Date().getFullYear()}
+    </footer>
+
+  </div>
+);
+}
---
+++ src/components/ProdutoCard.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+type ProdutoCardProps = {
+  nome: string;
+  imagem: string;
+  precoPequena: number;
+  precoGrande: number;
+  id: number;
+};
+
+export default function ProdutoCard({
+  nome,
+  imagem,
+  precoPequena,
+  precoGrande,
+  id,
+}: ProdutoCardProps) {
+  return (
+    <div className="bg-white p-4 rounded-md flex flex-col gap-4 border-none shadow-sm hover:shadow-md transition-shadow">
+      <Image
+        src={imagem}
+        alt={nome}
+        width={200}
+        height={200}
+        className="rounded-full"
+      />
+
+      <p className="font-sans text-sm">{nome}</p>
+      <p className="font-sans text-sm">
+        Pequena: R$ {precoPequena.toFixed(2)}
+      </p>
+      <p className="font-sans text-sm">
+        Grande: R$ {precoGrande.toFixed(2)}
+      </p>
+
+      <Link
+        href={`/pedido?id=${id}`}
+        className="text-white rounded-full p-2 w-24 text-center bg-emerald-600 hover:bg-emerald-700"
+      >
+        Comprar
+      </Link>
+    </div>
+  );
+}
